361403d5f2c59de3fc585baad6155d2e808c9cac
Chuck Driesler <charles.driesler@wework.com>
2019-10-03 14:22:26 -0400

    add new polyline builder

---
+++ src/builders/PolylineBuilder.ts
@@ -0,0 +1,49 @@
+import SvgarPath from './../models/SvgarPath';
+
+export default class PolylineBuilder {
+
+    private coordinates: number[];
+    
+    constructor(startPoint: number[]) {
+        this.coordinates = [startPoint[0], startPoint[1]];
+    }
+
+    public Build(): SvgarPath {
+        let c: number[] = [];
+
+        for (let i = 0; i < this.coordinates.length - 2; i += 2) {
+            let xA = this.coordinates[i];
+            let yA = this.coordinates[i + 1];
+            let xB = this.coordinates[i + 2];
+            let yB = this.coordinates[i + 3];
+
+            let xMid = (xA + xB) / 2;
+            let yMid = (yA + yB) / 2;
+
+            c = c.concat([xA, yA, xMid, yMid, xMid, yMid, xB, yB])
+        }
+
+        return new SvgarPath(c);
+    }
+
+    public MoveTo(x: number, y: number): PolylineBuilder {
+        this.coordinates.concat([x, y]);
+        return this;
+    }
+
+    public VerticalTo(y: number): PolylineBuilder {
+        this.coordinates = this.coordinates.concat([
+            this.coordinates[this.coordinates.length - 2], 
+            y
+        ]);
+        return this;
+    }
+
+    public HorizontalTo(x: number): PolylineBuilder {
+        this.coordinates = this.coordinates.concat([
+            x,
+            this.coordinates[this.coordinates.length - 1]
+        ]);
+        return this;
+    }
+}
