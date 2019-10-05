b3d424cdd8cc2f9b5283f3ec60682cd9c1e33e8c
Chuck <cdriesler.iv@gmail.com>
2019-10-04 22:41:48 -0400

    add builders to module export

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 import Cube from './models/SvgarCube';
 import Slab from './models/SvgarSlab';
 import Path from './models/SvgarPath';
+import CircleBuilder from './builders/CircleBuilder';
+import PolylineBuilder from './builders/PolylineBuilder';
+
+const Builder = {
+    Circle: CircleBuilder,
+    Polyline: PolylineBuilder
+}
 
 // Default model and idiom exports
 export default {
     Cube,
     Slab,
     Path,
+    Builder
 }
 
 // Individual schema model exports
