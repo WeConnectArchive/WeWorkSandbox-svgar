c350913b36b53db122440ad9d9d0989fe8cdf09d
Chuck Driesler <cdriesler.iv@gmail.com>
2019-11-13 21:54:06 -0500

    add coordinate mapping function

--- src/models/SvgarCube.ts
+++ src/models/SvgarCube.ts
@@ -1,5 +1,6 @@
 import SvgarSlab from './SvgarSlab';
 import Locate from './../predicates/Locate';
+import { newGuid } from './../utils/Guid';
 
 interface SvgarCubeCache {
     root: string,
@@ -27,6 +28,7 @@ export default class SvgarCube {
     };
 
     private name: string;
+    private id: string; 
 
     // Current svgar cache
     private cache: SvgarCubeCache = {
@@ -52,6 +54,7 @@ export default class SvgarCube {
         }
 
         this.name = name;
+        this.id = newGuid();
     }
 
     // Write out current state of svgar data as svg markup
@@ -63,6 +66,7 @@ export default class SvgarCube {
             this.cache.root = [
                 `<svg xmlns="http://www.w3.org/2000/svg"`, 
                 `viewBox="${this.scope.minimum[0]} ${-this.scope.maximum[1]} ${this.scope.maximum[0] - this.scope.minimum[0]} ${this.scope.maximum[1] - this.scope.minimum[1]}"`,
+                `id="${this.id}"`,
                 `width="${width}" height="${height}">\n`
             ].join(" ");
         }
@@ -175,5 +179,77 @@ export default class SvgarCube {
                 x.addEventListener(e, events[e])
             });
         })
+    }
+
+    // Map page coordinate to svgar coordinate by normalizing to parent div dimensions
+    mapPageCoordinateToSvgarCoordinate(pageX: number, pageY: number, div?: Element): number[] {
+        // Locate parent div with svgar coordinate or use provided element
+        var nx, ny;
+        const el: Element | null = div ?? document.querySelector(`#${this.id}`);
+
+        if (el == null) {
+            return [];
+        }
+
+        // Get normalized coordinate locations on html element
+        const svgarRect = el.getBoundingClientRect();
+        const nX = (pageX - svgarRect.left) / (svgarRect.right - svgarRect.left);
+        const nY = (pageY - svgarRect.top) / (svgarRect.bottom - svgarRect.top);
+
+        // Compare page div proportion to svgar camera proportion
+        const camera = this.scope;
+        const svgarXDomain = camera.maximum[0] - camera.minimum[0];
+        const svgarYDomain = camera.maximum[1] - camera.minimum[1];
+
+        const svgarProportion = svgarXDomain / svgarYDomain;
+
+        const pageXDomain = svgarRect.right - svgarRect.left;
+        const pageYDomain = svgarRect.bottom - svgarRect.top;
+
+        const pageProportion = pageXDomain / pageYDomain;
+
+        // Calculate svgar domain modifiers based on relative proportions
+        const xModifier = pageProportion > svgarProportion 
+            ? (pageXDomain / svgarXDomain) * (svgarYDomain / pageYDomain)
+            : 1;
+        const yModifier = pageProportion < svgarProportion
+            ? (pageYDomain / svgarYDomain) * (svgarXDomain / pageXDomain)
+            : 1;
+
+        // Calculate visible svgar extents
+        // ( Chuck ) This process is necessary given how svg renders a viewBox with different proportions than the parent div
+        interface SvgarExtents {
+            x: {
+                min: number,
+                max: number
+            },
+            y: {
+                min: number,
+                max: number
+            }
+        }
+
+        const deltaX = ((svgarXDomain * xModifier) - svgarXDomain) / 2;
+        const deltaY = ((svgarYDomain * yModifier) - svgarYDomain) / 2;
+
+        const visibleSvgarExtents: SvgarExtents = {
+            x: {
+                min: camera.minimum[0] - deltaX,
+                max: camera.maximum[0] + deltaX
+            },
+            y: {
+                min: camera.minimum[1] - deltaY,
+                max: camera.maximum[1] + deltaY
+            }
+        }
+
+        const visibleSvgarXDomain = visibleSvgarExtents.x.max - visibleSvgarExtents.x.min;
+        const visibleSvgarYDomain = visibleSvgarExtents.y.max - visibleSvgarExtents.y.min;
+
+        // Map normalized coordinates to calculated visible svgar domain
+        const svgarX = (nX * visibleSvgarXDomain) + visibleSvgarExtents.x.min;
+        const svgarY = visibleSvgarYDomain - ((nY * visibleSvgarYDomain) - visibleSvgarExtents.y.min);
+        
+        return [svgarX, svgarY];
     }
 }
